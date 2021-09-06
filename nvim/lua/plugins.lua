e64c04689996c63b8e64e1797314bc6be29fe2ee
pixelunknown <lancellottimattia@gmail.com>
2021-09-06 17:22:40 +0200

    Useless tabs deleted

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,27 +3,34 @@
 vim.cmd('packadd packer.nvim')
 
 return require('packer').startup({function()
-    -- Packer to manage itself
-    use 'wbthomason/packer.nvim'
+   -- Packer to manage itself
+   use 'wbthomason/packer.nvim'
 
-    -- My hundreds of colorschemes
-    use 'cocopon/iceberg.vim'
-    use 'kadekillary/Turtles'
-    use 'nightsense/cosmic_latte'
-    use 'doums/darcula'
-    use 'Mofiqul/dracula.nvim'
-    use 'glepnir/zephyr-nvim'
-    use 'whatyouhide/vim-gotham'
-    use 'arcticicestudio/nord-vim'
+   -- My hundreds of colorschemes
+   use 'cocopon/iceberg.vim'
+   use 'kadekillary/Turtles'
+   use 'nightsense/cosmic_latte'
+   use 'doums/darcula'
+   use 'Mofiqul/dracula.nvim'
+   use 'glepnir/zephyr-nvim'
+   use 'whatyouhide/vim-gotham'
+   use 'arcticicestudio/nord-vim'
 
-    -- Telescope and its requirments
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {{ 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' }}
-    }
+   -- Telescope:
+   --   A plugin for fuzzy finding stuff (especially files). It's written in
+   --   lua and requires 2 libraries from the nvim-lua git repository:
+   --      - popup for drawing windows;
+   --      - plenary for useful functions.
+   use {
+      'nvim-telescope/telescope.nvim',
+      requires = {{ 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' }},
+   }
 
-    -- Popup library
-    use 'nvim-lua/popup.nvim'
+   -- Popup library
+   use {
+      'nvim-lua/popup.nvim',
+      requires = { 'nvim-lua/plenary.nvim' }
+   }
 end,
 config = {
    display = {
