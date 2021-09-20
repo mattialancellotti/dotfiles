dfca573cb63dd3d0e24fd0a1433dbcb1a360ad21
pixelunknown <lancellottimattia@gmail.com>
2021-09-20 14:34:40 +0200

    Moving through a playlist of images using l and h

--- mpv/scripts/imv.lua
+++ mpv/scripts/imv.lua
@@ -4,6 +4,7 @@ require 'mp.options'
 local prop_get = mp.get_property
 local prop_set = mp.set_property
 local evnt_set = mp.register_event
+local bind_key = mp.add_key_binding
 
 -- When reading an image mpv will just open the file and close it immediately
 -- after. This function will be called avery time a file gets loaded and if it's
@@ -16,3 +17,8 @@ evnt_set("file-loaded", (function(event)
       prop_set("pause", "yes")
    end
 end))
+
+-- Mapping hl to move through the playlist using `playlist-next and
+-- playlist-prev`
+bind_key('l', (function() mp.command("playlist-next") end))
+bind_key('h', (function() mp.command("playlist-prev") end))
