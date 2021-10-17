47c496d3f40fb38753fbe275bbffef0c0adf4cd4
pixelunknown <lancellottimattia@gmail.com>
2021-10-17 21:00:53 +0200

    Add: showing image's name

--- mpv/mpv/scripts/imv.lua
+++ mpv/mpv/scripts/imv.lua
@@ -1,3 +1,12 @@
+-- TODO:
+--    + Need to create an ImageViewer class so that I can better organize the
+--      code, like creating lua tables to map actions like zooming-in/out to
+--      keys without rewriting the function every time;
+--    + Proper OSC not that mpv crap (do not take that as an insult);
+--    + Cropping the image would be cool;
+--    + Blurring runtime would be cool;
+--    + Collection project.
+--
 require 'mp'
 require 'mp.options'
 
@@ -19,7 +28,17 @@ evnt_set("file-loaded", (function(event)
       prop_set("pause", "yes")
       prop_set("osc", "no")
 
-      -- Initiating the veiwer
+      -- Showing filename
+      osc_msgs(prop_get("filename"))
+
+      -- Short function to update a property and eventually display a message
+      local function update(opt, update, msg)
+         prop_set(opt, prop_get(opt) + update)
+
+         if msg ~= nil then osc_msgs(msg) end
+      end
+
+      -- Initiating the viewer
       -- Mapping hl to move through the playlist using `playlist-next and
       -- playlist-prev`
       bind_key('l', (function() mp.command("playlist-next") end))
@@ -30,32 +49,16 @@ evnt_set("file-loaded", (function(event)
       --    + Zooming-out using '-';
       --    + Deafult zoom value using '='.
       bind_key('=', (function() prop_set("video-zoom", "0") end))
-      bind_key('+', (function()
-                        prop_set("video-zoom", (prop_get("video-zoom") + 0.1))
-                        osc_msgs("Zooming-in")
-                     end))
-      bind_key('-', (function()
-                        prop_set("video-zoom", (prop_get("video-zoom") - 0.1))
-                        osc_msgs("Zooming-out")
-                     end))
+      bind_key('+', (function() update("video-zoom",  0.1, "Zooming-in")  end))
+      bind_key('-', (function() update("video-zoom", -0.1, "Zooming-out") end))
+      bind_key('WHEEL_UP',
+                    (function() update("video-zoom",  0.1, "Zooming-int") end))
 
       -- Mapping HJKL to move through the zoomed image using `video-pan-x` and
       -- `video-pan-y`.
-      bind_key('H', (function()
-                        prop_set("video-pan-x",
-                                    (prop_get("video-pan-x") + 0.01))
-                     end))
-      bind_key('L', (function()
-                        prop_set("video-pan-x",
-                                    (prop_get("video-pan-x") - 0.01))
-                     end))
-      bind_key('J', (function()
-                        prop_set("video-pan-y",
-                                    (prop_get("video-pan-y") - 0.01))
-                     end))
-      bind_key('K', (function()
-                        prop_set("video-pan-y",
-                                    (prop_get("video-pan-y") + 0.01))
-                     end))
+      bind_key('H', (function() update("video-pan-x",  0.01) end))
+      bind_key('L', (function() update("video-pan-x", -0.01) end))
+      bind_key('J', (function() update("video-pan-y", -0.01) end))
+      bind_key('K', (function() update("video-pan-y",  0.01) end))
    end
 end))
