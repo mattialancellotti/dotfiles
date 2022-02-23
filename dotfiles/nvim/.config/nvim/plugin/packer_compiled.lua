cc2fd93201e04a2dfc3c7f0a69a88d924e1be7de
mattia <lancellottimattia@protonmail.com>
2022-02-23 19:19:21 +0100

    Idk plugin manager doing stuff

--- dotfiles/nvim/.config/nvim/plugin/packer_compiled.lua
+++ dotfiles/nvim/.config/nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/mattia/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?.lua;/home/mattia/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?/init.lua;/home/mattia/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?.lua;/home/mattia/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/mattia/.cache/nvim/packer_hererocks/2.0.5/lib/lua/5.1/?.so"
+local package_path_str = "/home/mattia/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/mattia/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/mattia/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/mattia/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/mattia/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -115,12 +115,13 @@ _G.packer_plugins = {
     url = "https://github.com/arcticicestudio/nord-vim"
   },
   ["nvim-tree.lua"] = {
+    config = { "\27LJ\2\n[\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\18disable_netrw\2\17hijack_netrw\2\nsetup\14nvim-tree\frequire\0" },
     loaded = true,
     path = "/home/mattia/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
-    config = { "\27LJ\1\2Ñ\4\0\0\4\0\25\0!4\0\0\0%\1\1\0>\0\2\0027\0\2\0>\0\1\0023\1\a\0003\2\4\0003\3\5\0:\3\6\2:\2\b\0013\2\t\0:\2\n\1:\1\3\0003\1\14\0003\2\f\0003\3\r\0:\3\6\2:\2\b\0013\2\15\0:\2\n\1:\1\v\0004\1\0\0%\2\16\0>\1\2\0027\1\17\0013\2\19\0003\3\18\0:\3\20\0023\3\21\0:\3\22\0023\3\23\0:\3\24\2@\1\2\0\vindent\1\0\1\venable\1\14highlight\1\0\2&additional_vim_regex_highlighting\1\venable\2\21ensure_installed\1\0\0\1\15\0\0\6c\bcpp\tjava\15dockerfile\blua\ago\vracket\nlatex\vpython\bvim\tbash\fcomment\tmake\15commonlisp\nsetup\28nvim-treesitter.configs\1\2\0\0\vracket\1\0\1\rfiletype\vracket\1\2\0\0\17src/parser.c\1\0\1\burl\30~/code/tree-sitter-racket\vracket\fused_by\1\2\0\0\tmake\17install_info\1\0\1\rfiletype\tmake\nfiles\1\2\0\0\17src/parser.c\1\0\1\burl\28~/code/tree-sitter-make\tmake\23get_parser_configs\28nvim-treesitter.parsers\frequire\0" },
+    config = { "\27LJ\2\nâ\3\0\0\5\0\20\0\0256\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0025\1\a\0005\2\4\0005\3\5\0=\3\6\2=\2\b\0015\2\t\0=\2\n\1=\1\3\0006\1\0\0'\3\v\0B\1\2\0029\1\f\0015\3\14\0005\4\r\0=\4\15\0035\4\16\0=\4\17\0035\4\18\0=\4\19\3D\1\2\0\vindent\1\0\1\venable\1\14highlight\1\0\2\venable\2&additional_vim_regex_highlighting\1\21ensure_installed\1\0\0\1\15\0\0\6c\bcpp\tjava\15dockerfile\blua\ago\vracket\nlatex\vpython\bvim\tbash\fcomment\tmake\15commonlisp\nsetup\28nvim-treesitter.configs\fused_by\1\2\0\0\vracket\17install_info\1\0\1\rfiletype\vracket\nfiles\1\2\0\0\17src/parser.c\1\0\1\burl\30~/code/tree-sitter-racket\vracket\23get_parser_configs\28nvim-treesitter.parsers\frequire\0" },
     loaded = true,
     path = "/home/mattia/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
@@ -141,21 +142,14 @@ _G.packer_plugins = {
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["popup.nvim"] = {
-    loaded = false,
-    needs_bufread = false,
-    only_cond = false,
-    path = "/home/mattia/.local/share/nvim/site/pack/packer/opt/popup.nvim",
+    loaded = true,
+    path = "/home/mattia/.local/share/nvim/site/pack/packer/start/popup.nvim",
     url = "https://github.com/nvim-lua/popup.nvim"
   },
   ["siena.vim"] = {
     loaded = true,
     path = "/home/mattia/.local/share/nvim/site/pack/packer/start/siena.vim",
     url = "https://github.com/brettbar/siena.vim"
-  },
-  ["simple-session-manager"] = {
-    loaded = true,
-    path = "/home/mattia/.local/share/nvim/site/pack/packer/start/simple-session-manager",
-    url = "https://github.com/mattialancellotti/simple-session-manager"
   },
   ["telescope.nvim"] = {
     loaded = true,
@@ -175,38 +169,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
-local module_lazy_loads = {
-  ["^popup%.nvim"] = "popup.nvim"
-}
-local lazy_load_called = {['packer.load'] = true}
-local function lazy_load_module(module_name)
-  local to_load = {}
-  if lazy_load_called[module_name] then return nil end
-  lazy_load_called[module_name] = true
-  for module_pat, plugin_name in pairs(module_lazy_loads) do
-    if not _G.packer_plugins[plugin_name].loaded and string.match(module_name, module_pat) then
-      to_load[#to_load + 1] = plugin_name
-    end
-  end
-
-  if #to_load > 0 then
-    require('packer.load')(to_load, {module = module_name}, _G.packer_plugins)
-    local loaded_mod = package.loaded[module_name]
-    if loaded_mod then
-      return function(modname) return loaded_mod end
-    end
-  end
-end
-
-if not vim.g.packer_custom_loader_enabled then
-  table.insert(package.loaders, 1, lazy_load_module)
-  vim.g.packer_custom_loader_enabled = true
-end
-
 -- Config for: nvim-treesitter
 time([[Config for nvim-treesitter]], true)
-try_loadstring("\27LJ\1\2Ñ\4\0\0\4\0\25\0!4\0\0\0%\1\1\0>\0\2\0027\0\2\0>\0\1\0023\1\a\0003\2\4\0003\3\5\0:\3\6\2:\2\b\0013\2\t\0:\2\n\1:\1\3\0003\1\14\0003\2\f\0003\3\r\0:\3\6\2:\2\b\0013\2\15\0:\2\n\1:\1\v\0004\1\0\0%\2\16\0>\1\2\0027\1\17\0013\2\19\0003\3\18\0:\3\20\0023\3\21\0:\3\22\0023\3\23\0:\3\24\2@\1\2\0\vindent\1\0\1\venable\1\14highlight\1\0\2&additional_vim_regex_highlighting\1\venable\2\21ensure_installed\1\0\0\1\15\0\0\6c\bcpp\tjava\15dockerfile\blua\ago\vracket\nlatex\vpython\bvim\tbash\fcomment\tmake\15commonlisp\nsetup\28nvim-treesitter.configs\1\2\0\0\vracket\1\0\1\rfiletype\vracket\1\2\0\0\17src/parser.c\1\0\1\burl\30~/code/tree-sitter-racket\vracket\fused_by\1\2\0\0\tmake\17install_info\1\0\1\rfiletype\tmake\nfiles\1\2\0\0\17src/parser.c\1\0\1\burl\28~/code/tree-sitter-make\tmake\23get_parser_configs\28nvim-treesitter.parsers\frequire\0", "config", "nvim-treesitter")
+try_loadstring("\27LJ\2\nâ\3\0\0\5\0\20\0\0256\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0025\1\a\0005\2\4\0005\3\5\0=\3\6\2=\2\b\0015\2\t\0=\2\n\1=\1\3\0006\1\0\0'\3\v\0B\1\2\0029\1\f\0015\3\14\0005\4\r\0=\4\15\0035\4\16\0=\4\17\0035\4\18\0=\4\19\3D\1\2\0\vindent\1\0\1\venable\1\14highlight\1\0\2\venable\2&additional_vim_regex_highlighting\1\21ensure_installed\1\0\0\1\15\0\0\6c\bcpp\tjava\15dockerfile\blua\ago\vracket\nlatex\vpython\bvim\tbash\fcomment\tmake\15commonlisp\nsetup\28nvim-treesitter.configs\fused_by\1\2\0\0\vracket\17install_info\1\0\1\rfiletype\vracket\nfiles\1\2\0\0\17src/parser.c\1\0\1\burl\30~/code/tree-sitter-racket\vracket\23get_parser_configs\28nvim-treesitter.parsers\frequire\0", "config", "nvim-treesitter")
 time([[Config for nvim-treesitter]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\2\n[\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\18disable_netrw\2\17hijack_netrw\2\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
 if should_profile then save_profiles() end
 
 end)
