fa2d35f1ffb96dc3306b9d5fb657aa0304ea4a92
mattia <lancellottimattia@protonmail.com>
2022-07-10 18:26:05 +0200

    Adding bluetooth component, custom/bluetooth now used to highlights

--- dotfiles/waybar/.config/waybar/scripts/bluecheck.sh
+++ dotfiles/waybar/.config/waybar/scripts/bluecheck.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Saving bluetooth device's MAC address
-IFS=$'\n'
-PAIRED_DEVICES=($(bluetoothctl paired-devices | cut -d ' ' -f2))
-CONNECTED_DEVICE=$(while read device; do bluetoothctl info $device; done <<< ${PAIRED_DEVICES[*]} \
+MAC="F4:73:35:77:D4:92"
+DEVICE="Logitech Keyboard K380"
+
+CONNECTED_DEVICE=$(bluetoothctl info $MAC \
   | grep "Name:\|Connected: yes" \
   | grep -B1 "yes" \
   | head -n 1 \
@@ -11,4 +12,9 @@ CONNECTED_DEVICE=$(while read device; do bluetoothctl info $device; done <<< ${P
 
 
 # Formatting the data using json syntax
-echo "{\"text\": \"$CONNECTED_DEVICE\", \"tooltip\": \"\", \"class\": \"\", \"percentage\": \"\"}"
+if [ -z "$CONNECTED_DEVICE" ];
+then
+  echo "{\"text\": \"$DEVICE\", \"tooltip\": \"\", \"class\": \"off\", \"percentage\": \"\"}"
+else
+  echo "{\"text\": \"$DEVICE\", \"tooltip\": \"\", \"class\": \"on\", \"percentage\": \"\"}"
+fi
