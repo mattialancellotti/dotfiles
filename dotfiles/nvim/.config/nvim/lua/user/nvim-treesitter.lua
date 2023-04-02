69e0ab294b53bf0c380da97387074f13e4c2aaf5
Mattia Lancellotti <lancellottimattia@pm.me>
2023-04-02 18:11:37 +0200

    Removing useless filetype

--- dotfiles/nvim/.config/nvim/lua/user/nvim-treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/user/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup({
    -- Ensuring some languages are installed
    ensure_installed = {
-      "awk", "dot", "c", "cpp", "dockerfile", "lua", "go", "scheme",
+      "awk", "c", "cpp", "dockerfile", "lua", "go", "scheme",
       "yaml", "json", "gitcommit", "gitattributes", "markdown_inline",
       "html", "latex", "python", "vim", "comment", "make", "c_sharp",
       "markdown", "bash", "toml", "todotxt", "rust"
