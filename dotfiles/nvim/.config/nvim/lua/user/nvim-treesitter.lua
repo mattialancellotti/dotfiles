ddbf85942f676167b3ffb586d2acdd57d65830ff
Mattia Lancellotti <lancellottimattia@protonmail.com>
2023-08-31 22:35:00 +0200

    Updating various configuration files and fixing some bugs

--- dotfiles/nvim/.config/nvim/lua/user/nvim-treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/user/nvim-treesitter.lua
@@ -2,9 +2,10 @@ require('nvim-treesitter.configs').setup({
    -- Ensuring some languages are installed
    ensure_installed = {
       "awk", "c", "cpp", "dockerfile", "lua", "go", "scheme",
-      "yaml", "json", "gitcommit", "gitattributes", "markdown_inline",
-      "html", "latex", "python", "vim", "comment", "make", "c_sharp",
-      "markdown", "bash", "toml", "todotxt", "rust"
+      "yaml", "json", "gitcommit", "gitignore", "gitattributes",
+      "git_config", "git_rebase", "markdown_inline", "html",
+      "latex", "python", "vim", "comment", "make", "c_sharp",
+      "markdown", "bash", "toml", "todotxt", "rust", "racket"
    },
    -- Configuring highlighting for all modules
    highlight = {
